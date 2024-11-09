0447ebc76333725237e5a23b81f232a623a22fcd
GeraZorzoni <gzorzoni@gmail.com>
2024-11-09 12:13:28 +0100

    commmit before angular changes version

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+//import { BaseChartDirective } from 'ng2-charts';
+
 import { AppRoutingModule } from '../app-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { ComponentsModule } from '../components/components.module';
